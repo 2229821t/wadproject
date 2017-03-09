9293b9427d59c0ea5ebf06457425df7fba4d34a7
Yaoi Tseng <2229821t@student.gla.ac.uk>
2017-03-09 19:22:50 +0000

    09032017

--- LanguageExchange/admin.py
+++ LanguageExchange/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from LanguageExchange.models import User, Language, Country
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Language)
+admin.site.register(Country)
--- LanguageExchange/models.py
+++ LanguageExchange/models.py
@@ -2,4 +2,29 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length = 128, unique = True, null = False)
+    GUID = models.IntegerField(unique = True, null = False)
+    email = models.EmailField(max_length = 128, unique = True, null = False)
+    password = models.CharField(max_length = 16, null = False)
+    nationality = models.CharField(max_length = 128, null = False)
+    NLanguge = models.CharField(max_length = 128, null = False)
+    NLanguge2 = models.CharField(max_length = 128)
+    LLanguge = models.CharField(max_length = 128, null = False)
+    LLanguge2 = models.CharField(max_length = 128)
+
+    def _str_(self):
+        return name
+
+class Language(models.Model):
+    language = models.CharField(max_length = 128, unique = True, null = False)
+
+    def _str_(self):
+        return language
+
+class Country(models.Model):
+    country = models.CharField(max_length = 128, unique = True, null = False)
+
+    def _str_(self):
+        return country
+
